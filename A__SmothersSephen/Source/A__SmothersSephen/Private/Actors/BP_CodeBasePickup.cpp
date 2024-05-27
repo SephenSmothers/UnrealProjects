336f597d3a3704c803a8f3cd8387f609e2b2978f
SephenSmothers <sephensmothers@yahoo.com>
2024-05-27 12:14:09 -0400

    week3 stuff

--- A__SmothersSephen/Source/A__SmothersSephen/Private/Actors/BP_CodeBasePickup.cpp
+++ A__SmothersSephen/Source/A__SmothersSephen/Private/Actors/BP_CodeBasePickup.cpp
@@ -47,8 +47,6 @@ void ABP_CodeBasePickup::HandlePostPickup()
 
 void ABP_CodeBasePickup::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	int test = 0;
-	test++;
 
 	//if (CanPickup(OtherActor))
 	//{
